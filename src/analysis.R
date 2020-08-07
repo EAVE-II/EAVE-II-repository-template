468d8ee1468074ad0d2c105da563261eac74b941
MulhollandRH <rachel.mulholland@ed.ac.uk>
2020-08-07 10:37:12 +0100

    Added header template into 'analysis.R' file

---
+++ src/analysis.R
@@ -0,0 +1,7 @@
+######################################################################
+## Title: Full title of paper
+## Short title: Short title of paper (if applicable)
+## DOI: DOI of published paper
+## Code author: Lead analyst name <Lead analyst email>
+## Description: Quick description on the code (e.g. Analysis code, data cleansing code)
+######################################################################
